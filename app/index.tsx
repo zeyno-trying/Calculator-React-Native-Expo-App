b5cfa6517ccff231842c5d1cede71d990f3c81f9
Zineb153 <zinebdjebbari610@gmail.com>
2025-02-01 02:31:07 +0100

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function HomeScreen() {
+ return(
+  <SafeAreaView className="flex-1 items-center justify-center bg-white">
+    <Text className="text-red-600 font-semibold">Heey zeyno its u setting up react native app on your own</Text>
+  </SafeAreaView>
+ )
+}
+
